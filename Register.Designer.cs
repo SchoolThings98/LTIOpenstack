4735263062b38eb57ea83985cdff99de5d4c1fc1
Victor Boarato <2181836@my.ipleiria.pt>
2021-04-11 18:08:15 +0100

    Controlo preenchimento dos campos
    Verificação se os campos tão preenchidos / password serem iguais

--- Register.Designer.cs
+++ Register.Designer.cs
@@ -42,10 +42,9 @@ namespace LTIOpenstackProject
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(241, 65);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(181, 53);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username";
             // 
@@ -53,10 +52,9 @@ namespace LTIOpenstackProject
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(241, 154);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(181, 125);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 25);
+            this.label2.Size = new System.Drawing.Size(78, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password";
             // 
@@ -64,44 +62,41 @@ namespace LTIOpenstackProject
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(204, 246);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(153, 200);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(171, 25);
+            this.label3.Size = new System.Drawing.Size(137, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Confirm Password";
             // 
             // textBoxUsername
             // 
-            this.textBoxUsername.Location = new System.Drawing.Point(209, 94);
-            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxUsername.Location = new System.Drawing.Point(157, 76);
             this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(176, 22);
+            this.textBoxUsername.Size = new System.Drawing.Size(133, 20);
             this.textBoxUsername.TabIndex = 3;
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(209, 182);
-            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxPassword.Location = new System.Drawing.Point(157, 148);
             this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(176, 22);
+            this.textBoxPassword.Size = new System.Drawing.Size(133, 20);
             this.textBoxPassword.TabIndex = 4;
+            this.textBoxPassword.UseSystemPasswordChar = true;
             // 
             // textBoxConfirmPassword
             // 
-            this.textBoxConfirmPassword.Location = new System.Drawing.Point(209, 274);
-            this.textBoxConfirmPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxConfirmPassword.Location = new System.Drawing.Point(157, 223);
             this.textBoxConfirmPassword.Name = "textBoxConfirmPassword";
-            this.textBoxConfirmPassword.Size = new System.Drawing.Size(176, 22);
+            this.textBoxConfirmPassword.Size = new System.Drawing.Size(133, 20);
             this.textBoxConfirmPassword.TabIndex = 5;
+            this.textBoxConfirmPassword.UseSystemPasswordChar = true;
             // 
             // btnRegister
             // 
             this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegister.Location = new System.Drawing.Point(209, 341);
-            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRegister.Location = new System.Drawing.Point(157, 277);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(177, 52);
+            this.btnRegister.Size = new System.Drawing.Size(133, 42);
             this.btnRegister.TabIndex = 6;
             this.btnRegister.Text = "Register User";
             this.btnRegister.UseVisualStyleBackColor = true;
@@ -109,9 +104,9 @@ namespace LTIOpenstackProject
             // 
             // Register
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 502);
+            this.ClientSize = new System.Drawing.Size(468, 408);
             this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.textBoxConfirmPassword);
             this.Controls.Add(this.textBoxPassword);
@@ -119,7 +114,6 @@ namespace LTIOpenstackProject
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Register";
             this.Text = "Register";
             this.ResumeLayout(false);
--- Register.cs
+++ Register.cs
@@ -19,7 +19,21 @@ namespace LTIOpenstackProject
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
+            if (textBoxUsername.Text != string.Empty || textBoxPassword.Text != string.Empty || textBoxConfirmPassword.Text != string.Empty)
+            {
+                if (textBoxPassword.Text == textBoxConfirmPassword.Text)
+                {
 
+                }
+                else
+                {
+                    MessageBox.Show("Password tem de ser iguais", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Preencha todos os campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
